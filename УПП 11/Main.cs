17c891d3b3b52ac7639dc03213b70fda4222e9ba
onetooz <lobatch.vania@gmail.com>
2024-11-20 19:47:05 +0300

    Finish

--- УПП 11/Main.cs
+++ УПП 11/Main.cs
@@ -1,5 +1,6 @@
 ﻿//using class_ResearchTeam;
 using System;
+using System.Diagnostics;
 using System.Net;
 using System.Runtime.CompilerServices;
 using УПП_11;
@@ -8,152 +9,121 @@ public class Programm
 {
     public static void Main()
     {
-        // 1. Создать объект ResearchTeam с вводом данных
-        Console.WriteLine("Создание объекта ResearchTeam:");
         Console.Write("Введите тему исследований: ");
         string theme = Console.ReadLine();
 
-        Console.Write("Введите название исследовательской организации: ");
+        Console.Write("Введите название организации: ");
         string orgName = Console.ReadLine();
 
         Console.Write("Введите регистрационный номер исследований: ");
         int regNum = int.Parse(Console.ReadLine());
 
-        Console.Write("Введите время, отведенное на исследования (0 - Year, 1 - TwoYears, 2 - Long): ");
-        TimeFrame time = (TimeFrame)int.Parse(Console.ReadLine());
+        Console.WriteLine("Выберите временной интервал (0 - Year, 1 - TwoYears, 2 - Long): ");
+        TimeFrame timeFrame = (TimeFrame)int.Parse(Console.ReadLine());
 
-        ResearchTeam researchTeam = new ResearchTeam(theme, orgName, regNum, time);
+        ResearchTeam researchTeam = new ResearchTeam(theme, orgName, regNum, timeFrame);
 
-        Console.WriteLine("\nКраткая информация об объекте ResearchTeam:");
-        Console.WriteLine(researchTeam.ToShortString());
+        Console.WriteLine("\nПроверка индексатора:");
+        foreach (TimeFrame frame in Enum.GetValues(typeof(TimeFrame)))
+        {
+            Console.WriteLine($"Для {frame}: {researchTeam[frame]}");
+        }
 
-        // 10. Вывод значений индексатора
-        Console.WriteLine("\nПроверка значений индексатора:");
-        Console.WriteLine($"TimeFrame.Year: {researchTeam[TimeFrame.Year]}");
-        Console.WriteLine($"TimeFrame.TwoYears: {researchTeam[TimeFrame.TwoYears]}");
-        Console.WriteLine($"TimeFrame.Long: {researchTeam[TimeFrame.Long]}");
-
-        // 11. Присвоить значения свойствам
-        Console.WriteLine("\nОбновление свойств ResearchTeam:");
-        Console.Write("Введите новую тему исследований: ");
+        Console.WriteLine("\nПрисваивание новых значений свойствам...");
+        Console.Write("Введите тему исследований: ");
         researchTeam.Theme = Console.ReadLine();
-
-        Console.Write("Введите новое название исследовательской организации: ");
+        Console.Write("Введите название организации: ");
         researchTeam.OrgName = Console.ReadLine();
-
-        Console.Write("Введите новый регистрационный номер исследований: ");
+        Console.Write("Введите регистрационный номер исследований: ");
         researchTeam.RegNum = int.Parse(Console.ReadLine());
-
-        Console.Write("Введите новое время, отведенное на исследования (0 - Year, 1 - TwoYears, 2 - Long): ");
+        Console.WriteLine("Выберите временной интервал (0 - Year, 1 - TwoYears, 2 - Long): ");
         researchTeam.Time = (TimeFrame)int.Parse(Console.ReadLine());
 
-        Console.WriteLine("\nПолная информация об объекте ResearchTeam:");
+        Console.WriteLine("\nПолная информация:");
         Console.WriteLine(researchTeam.ToString());
 
-        // 12. Добавление публикаций
-        Console.WriteLine("\nДобавление публикаций в ResearchTeam:");
+        Console.WriteLine("\nДобавление публикаций...");
         Console.Write("Сколько публикаций вы хотите добавить? ");
-        int paperCount = int.Parse(Console.ReadLine());
+        int numPapers = int.Parse(Console.ReadLine());
 
-        Paper[] papers = new Paper[paperCount];
-        for (int i = 0; i < paperCount; i++)
+        for (int i = 0; i < numPapers; i++)
         {
             Console.WriteLine($"\nДобавление публикации {i + 1}:");
-
-            Console.Write("Введите название публикации: ");
-            string title = Console.ReadLine();
-
-            Console.Write("Введите имя автора публикации: ");
-            string authorName = Console.ReadLine();
-
-            Console.Write("Введите фамилию автора публикации: ");
-            string authorSurname = Console.ReadLine();
-
-            Console.Write("Введите дату рождения автора (дд.мм.гггг): ");
-            DateTime birthDate = DateTime.Parse(Console.ReadLine());
-
-            Console.Write("Введите дату публикации (дд.мм.гггг): ");
-            DateTime publicationDate = DateTime.Parse(Console.ReadLine());
-
-            papers[i] = new Paper
-            {
-                title = title,
-                author = new Person(authorName, authorSurname, birthDate),
-                publicationDate = publicationDate
-            };
+            researchTeam.AddPaper();
         }
 
-        researchTeam.Publications = papers;
-
-        Console.WriteLine("\nИнформация после добавления публикаций:");
+        Console.WriteLine("\nДанные после добавления публикаций:");
         Console.WriteLine(researchTeam.ToString());
 
-        // 13. Публикация с самой поздней датой
-        Paper latestPaper = GetLatestPaper(researchTeam.Publications);
-        Console.WriteLine("\nСамая поздняя публикация:");
+ 
+        Paper latestPaper = FindLatestPublication(researchTeam.Publications);
         if (latestPaper != null)
         {
-            Console.WriteLine($"Название: {latestPaper.title}");
-            Console.WriteLine($"Автор: {latestPaper.author.Name} {latestPaper.author.Surname}");
-            Console.WriteLine($"Дата публикации: {latestPaper.publicationDate.ToShortDateString()}");
-        }
-        else
-        {
-            Console.WriteLine("Нет доступных публикаций.");
+            Console.WriteLine("\nСамая поздняя публикация:");
+            Console.WriteLine(latestPaper.ToString());
         }
 
-        // 14. Сравнение времени работы с массивами
+        Console.WriteLine("\nСравнение времени работы с массивами...");
         CompareArrayPerformance();
     }
 
-    // Метод для получения самой поздней публикации
-    static Paper GetLatestPaper(Paper[] publications)
+    static Paper FindLatestPublication(Paper[] publications)
     {
         if (publications == null || publications.Length == 0) return null;
 
         Paper latest = publications[0];
         foreach (var paper in publications)
         {
-            if (paper.publicationDate > latest.publicationDate)
+            if (paper != null && paper.publicationDate > latest.publicationDate)
                 latest = paper;
         }
         return latest;
     }
 
-    // Метод для сравнения времени работы с массивами
     static void CompareArrayPerformance()
     {
-        int size = 1000;
-        Paper[] oneDimensionalArray = new Paper[size];
-        Paper[,] twoDimensionalArray = new Paper[size, 1];
-        Paper[][] jaggedArray = new Paper[size][];
+        int size = 100000;
+        var one = new Paper[size];
+        var two = new Paper[100, 1000];
+        var Ar = new Paper[100][];
+        for (int i = 0; i < 100; i++) Ar[i] = new Paper[1000];
 
+
+        for (int i = 0; i < size; i++) one[i] = new Paper();
+        for (int i = 0; i < 100; i++)
+            for (int j = 0; j < 1000; j++)
+            {
+                two[i, j] = new Paper();
+                Ar[i][j] = new Paper();
+            }
+
+        var watch = new Stopwatch();
+
+
+        watch.Restart();
         for (int i = 0; i < size; i++)
         {
-            oneDimensionalArray[i] = new Paper();
-            twoDimensionalArray[i, 0] = new Paper();
-            jaggedArray[i] = new Paper[1];
-            jaggedArray[i][0] = new Paper();
+            var q = one[i];
         }
-
-        var watch = System.Diagnostics.Stopwatch.StartNew();
-
-        // Операции с одномерным массивом
-        watch.Restart();
-        for (int i = 0; i < size; i++) { var _ = oneDimensionalArray[i]; }
         watch.Stop();
-        Console.WriteLine($"\nВремя работы с одномерным массивом: {watch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"Время работы с одномерным массивом: {watch.ElapsedMilliseconds} ms");
 
-        // Операции с двумерным прямоугольным массивом
         watch.Restart();
-        for (int i = 0; i < size; i++) { var _ = twoDimensionalArray[i, 0]; }
+        for (int i = 0; i < 100; i++)
+            for (int j = 0; j < 1000; j++)
+            {
+                var q = two[i, j];
+            }
         watch.Stop();
         Console.WriteLine($"Время работы с двумерным массивом: {watch.ElapsedMilliseconds} ms");
 
-        // Операции с двумерным ступенчатым массивом
         watch.Restart();
-        for (int i = 0; i < size; i++) { var _ = jaggedArray[i][0]; }
+        for (int i = 0; i < 100; i++)
+            for (int j = 0; j < 1000; j++)
+            {
+                var q = Ar[i][j];
+            }
         watch.Stop();
-        Console.WriteLine($"Время работы с двумерным ступенчатым массивом: {watch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"Время работы с зубчатым массивом: {watch.ElapsedMilliseconds} ms");
     }
 }
