238da298527ae17bcb4c7733e29d0f60941a09b7
onetooz <lobatch.vania@gmail.com>
2024-11-20 13:45:47 +0300

    Main

--- УПП 11/Main.cs
+++ УПП 11/Main.cs
@@ -1,24 +1,31 @@
-﻿using class_ResearchTeam;
-using System;
-using System.Net;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Programm
+namespace УПП_11
 {
-    public static void Main()
+    internal class Main
     {
-        TimeFrame time = TimeFrame.Year;
-        int n = 1;
-        ResearchTeam bag = new ResearchTeam("Литералы", "Лобачи Интернешнл", 12, time);
-        DateTime birthday = new DateTime(2001, 07, 11);
-        DateTime pubtime = new DateTime(2020, 03, 23);
-        Person Vania = new Person("Vania", "Lobach", birthday);
-        Paper[] mas = new Paper[n];
-        for (int i = 0; i < n; i++)
+        public class Programm
         {
-            bag.AddPaper();
+            public static void Main()
+            {
+                TimeFrame time = TimeFrame.Year;
+                int n = 1;
+                ResearchTeam bag = new ResearchTeam("Литералы", "Лобачи Интернешнл", 12, time);
+                DateTime birthday = new DateTime(2001, 07, 11);
+                DateTime pubtime = new DateTime(2020, 03, 23);
+                Person Vania = new Person("Vania", "Lobach", birthday);
+                Paper[] mas = new Paper[n];
+                for (int i = 0; i < n; i++)
+                {
+                    bag.AddPaper();
+                }
+                Console.WriteLine(bag.ToString());
+                Console.WriteLine(bag[time]);
+            }
         }
-        Console.WriteLine(bag.ToString());
-        Console.WriteLine(bag[time]);
     }
 }
